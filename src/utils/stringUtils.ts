7a094e9156f01dce9ad37ea81b8f9218462d2a08
Đặng Trường Giang <giangdt.kma@gmail.com>
2021-06-25 00:17:35 +0700

    add function getInitialName

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,12 +1,20 @@
 import { addSeconds, format } from 'date-fns';
-import { User } from 'src/domain/user';
+
+interface NameUser {
+    name: string;
+}
 
 export const formatTimeDuration = (seconds: number): string => {
     const helperDate = addSeconds(new Date(0), seconds);
     return format(helperDate, 'mm:ss');
 };
 
-export const getLastName = (user?: User): string => {
-    if (!user) return '';
-    return user.name.split(' ').slice(-1)[0];
+export const getLastName = (item: NameUser): string => {
+    return item.name.split(' ').slice(-1)[0];
+};
+
+export const getInitialName = (item: NameUser): string => {
+    const arrName = item.name.split(' ');
+    if (arrName.length === 1) return arrName[0][0];
+    return `${arrName[arrName.length - 2][0]}${arrName[arrName.length - 1][0]}`;
 };
